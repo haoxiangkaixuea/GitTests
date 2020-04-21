20fea45c351c2992594e0b970c100ad838788e15
Roger <990545738@qq.com>
2020-04-21 14:05:26 +0800

    版本六：删除了一行代码

--- src/main/java/cn/edu/scujcc/model/test.java
+++ src/main/java/cn/edu/scujcc/model/test.java
@@ -3,7 +3,6 @@ package cn.edu.scujcc.model;
 public class test {
    public void tests() {
 	   System.out.println("哈哈哈哈哈哈");
-	   System.out.println("hhhhhhhh");
 	   System.out.println("hhhh呵呵呵呵呵");
    }
 }
